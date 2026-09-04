59c47b83444bce1babc8a1996c9d87094ec1f9cc
Fernando Aguirre <yassir468124@gmail.com>
2026-09-04 11:09:25 -0600

    Remove monthly offers flyer popup

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,6 @@
 import type { ReactNode } from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
-import FlyerPopup from './FlyerPopup';
 import Asistente from './Asistente';
 import { CartProvider } from '../context/CartContext';
 
@@ -9,7 +8,6 @@ export default function Layout({ children }: { children: ReactNode }) {
   return (
     <CartProvider>
       <>
-        <FlyerPopup />
         <Navbar />
         <main>{children}</main>
         <Footer />
